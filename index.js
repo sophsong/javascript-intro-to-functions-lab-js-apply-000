5f2483cdfc45bdf85b242c83e827a6958f98a60d
Sophia Song <sophia.song@tufts.edu>
2018-02-05 21:02:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ function logShout(string) {console.log; string.toUpperCase()}
 var lowerCase= "hello!"
 function sayHiToGrandma(string) {return string.toLowerCase() === string
 }
-if (string.toLowerCase() === string) {return; "I can't hear you!"}
-if (string === "I love you, Grandma.") {return; "I love you, too"}
-if (string.toUpperCase() === string) return; { "YES INDEED!"}
+if (string.toLowerCase() === string)  {
+  return "I can't hear you!" }
+if (string === "I love you, Grandma.") {
+  return "I love you, too"}
+if (string.toUpperCase() === string) {
+  return  "YES INDEED!"}
